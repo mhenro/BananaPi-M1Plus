4e16f9397848d32c53a027be2e91412c754c9bc4
Alexander Khadzhinov <mhenro@gmail.com>
2016-03-15 16:24:06 +0300

    Update create_img.sh
    1. Added full automation. Now script inputs data to stdin itself.
    2. Added progressvar feature that basing on pv shell-command

--- create_img.sh
+++ create_img.sh
@@ -2,10 +2,16 @@
 
 #declare variables
 IMG_FILE="vtr100.img"
-IMG_SIZE=350	#MB
+IMG_SIZE=400	#MB
+PASSWORD="Y9Jx1psL"
+
+#function to copy files with show progress
+cpStat () {
+	tar c "$1" | pv -s ${IMG_SIZE}M | tar x -C "$2"
+}
 
 echo "Creating img file..."
-dd if=/dev/zero of=${IMG_FILE} bs=1M count=${IMG_SIZE}
+dd if=/dev/zero bs=1M count=${IMG_SIZE} | pv -s ${IMG_SIZE}M > ${IMG_FILE}
 LOOP_DEV=`losetup -f --show ${IMG_FILE}`
 LOOP_PART_BOOT="${LOOP_DEV}p1"
 LOOP_PART_SYS="${LOOP_DEV}p2"
@@ -26,27 +32,42 @@ mkfs.vfat ${LOOP_PART_BOOT}
 
 echo "Copying files to boot partition..."
 mount ${LOOP_PART_BOOT} /mnt/
-cp bootloader/uImage /mnt/
-cp bootloader/uInitrd /mnt/
-cp bootloader/script.bin /mnt/
-cp bootloader/boot.scr /mnt/
+pv bootloader/uImage > /mnt/uImage
+pv bootloader/uInitrd > /mnt/uInitrd
+pv bootloader/script.bin > /mnt/script.bin
+pv bootloader/boot.scr > /mnt/boot.scr
 umount /mnt/
 
 echo "Create rootfs partition..."
-cryptsetup -y --cipher aes-xts-plain --key-size 256 luksFormat ${LOOP_PART_SYS}
-cryptsetup luksOpen ${LOOP_PART_SYS} rootfs
+cat <<EOT | cryptsetup --cipher aes-xts-plain --key-size 256 luksFormat ${LOOP_PART_SYS}
+$PASSWORD
+EOT
+cat <<EOT | cryptsetup luksOpen ${LOOP_PART_SYS} rootfs
+$PASSWORD
+EOT
 
 echo "Formatting rootfs partition..."
 mkfs.ext4 /dev/mapper/rootfs
 
 echo "Copying files to rootfs partition..."
 mount /dev/mapper/rootfs /mnt/
-cp -r rootfs/* /mnt/
+cpStat rootfs/ /mnt/
 umount /mnt/
 
 echo "Add crypt key to initramfs..."
-cryptsetup luksAddKey ${LOOP_PART_SYS} initramfs/etc/password
+cat <<EOT | cryptsetup luksAddKey ${LOOP_PART_SYS} initramfs/etc/password
+$PASSwORD
+EOT
 cryptsetup luksClose rootfs
+
+echo "Create initramfs..."
+sh create_initramfs.sh
+mv uInitrd bootloader/
+rm initramfs.cpio
+rm initramfs.cpio.gz
+mount ${LOOP_PART_BOOT} /mnt/
+pv bootloader/uInitrd > /mnt/uInitrd
+umount /mnt/
 
 losetup -d ${LOOP_DEV}
 
